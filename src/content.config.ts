2670453ad419f031504aff96a47f93dee00b0454
Joe Starr <joe@joe-starr.com>
2026-09-21 17:05:57 -0400

    FEAT: Moves kitw content to its own section. Adds RSS feeds

--- src/content.config.ts
+++ src/content.config.ts
@@ -33,6 +33,28 @@ const postsCollection = defineCollection({
         }),
 });
 
+const kitwCollection = defineCollection({
+    loader: glob({ pattern: ["**/*.md", "**/*.mdx"], base: "./src/content/kitw" }),
+    schema: ({ image }) =>
+        z.object({
+            title: z.string(),
+            published: z.coerce.date(),
+            // updated: z.coerce.date().optional(),
+            draft: z.boolean().optional().default(false),
+            description: z.string().optional(),
+            author: z.string().optional(),
+            series: z.string().optional(),
+            tags: z.array(z.string()).optional().default([]),
+            coverImage: z
+                .strictObject({
+                    src: image(),
+                    alt: z.string(),
+                })
+                .optional(),
+            toc: z.boolean().optional().default(true),
+        }),
+});
+
 const homeCollection = defineCollection({
     loader: glob({ pattern: ["home.md", "home.mdx"], base: "./src/content" }),
     schema: ({ image }) =>
@@ -159,4 +181,5 @@ export const collections = {
     gentree: gentreeCollection,
     cv: cvCollection,
     resume: resumeCollection,
+    kitw: kitwCollection,
 };
